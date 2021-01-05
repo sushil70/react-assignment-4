cd757bdd22f85b4a0bb7d9f6629bfa1446fd1054
sushil07 <sushil29121997@gmail.com>
2021-01-05 11:58:36 +0530

    4

---
+++ react-assignment-4/src/header.js
@@ -0,0 +1,11 @@
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return <h1>This Header</h1>
+    }
+}
+
+export default Header
