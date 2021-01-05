5ee5f93d0e331cfa0ee339fd7ea67ff229bce7e3
sushil07 <sushil29121997@gmail.com>
2021-01-05 13:05:56 +0530

    5

--- react-assignment-4/src/header.js
+++ react-assignment-4/src/header.js
@@ -1,10 +1,31 @@
+import React, {Component} from "react"
+import "./header.css"
+
 class Header extends Component {
     constructor(props) {
         super(props)
         this.state = {}
     }
     render() {
-        return <h1>This Header</h1>
+        return (
+            <>
+                <div className="header-main">
+                    <div className="header">
+                        <div className="logo">
+                            <a href="#" className="logo-link">
+                                <img
+                                    src="https://assets.website-files.com/5e7ff3ec0c4ef4c974fa99e3/5e7ff57adad44d1f072965b6_logo.svg"
+                                    alt="logo"
+                                />
+                            </a>
+                        </div>
+                        <div className="nav">
+                            <span className="nav-item">Home</span>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
     }
 }
 
